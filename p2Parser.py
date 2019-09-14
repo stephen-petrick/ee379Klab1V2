51b4936dc1fa16a222791aaf8bcee02a3c555948
SP <stephenp1912@gmail.com>
2019-09-14 01:01:12 -0500

    added parser script for p2.2

--- p2Parser.py
+++ p2Parser.py
@@ -1,15 +1,49 @@
 from ipwhois import IPWhois
-import pprint
+from ipwhois.utils import unique_addresses
+from pprint import pprint
 
 """Includes new lines
 fileReader = open("results.csv", "r")
 print ("First IP Address: ")
 print fileReader.readline()
 """
-lines = open("results.csv", "r")
-lines = [lines.rstrip('\n') for line in open ('results.csv')]
-lines.readline()
+#lines = open("results.csv", "r")
+lines = [line.rstrip('\n') for line in open ('results.csv')]
+#lines.readline()
+#print(lines)
+print(lines[0])
 
-whoisObj = IPWhois(lines.readline())
-results = obj.lookup_rws()
-print(results['nets'][0]['name'])
+#writer = open("subnetCIDRs.txt","w")
+index = 0
+subnets = ''
+
+#Parse through IP addresses from .csv file and puts them into subnetCIDRs.txt
+"""
+writer = open("subnetCIDRs.txt","w")
+for x in lines:
+	whoisObj = IPWhois(lines[index])
+	results = whoisObj.lookup_rdap()
+	#print(results)
+	print(results['asn_cidr'])
+	writer.write(results['asn_cidr']+",")
+	index += 1
+writer.close()
+"""
+""" same as above but putting into a string instead of a text file
+for x in lines:
+	whoisObj = IPWhois(lines[index])
+	results = whoisObj.lookup_rdap()
+	print(results['asn_cidr'])
+	subnets += results['asn_cidr']+","
+	index += 1
+"""
+
+fileReader = open("subnetCIDRs2.txt", "r")
+subnets = fileReader.readline()
+#print(subnets)
+
+subnetCount = unique_addresses(data=subnets, file_path = None)
+pprint(subnetCount)
+
+#writer.close()
+fileReader.close()
